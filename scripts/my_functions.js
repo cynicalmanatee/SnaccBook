46fbf24da2fad5482719e1a6d5f8ea3acd51cde3
Steven Nguyen <sn.nguyen72@gmail.com>
2021-03-25 14:06:27 -0700

    made it so the post is linked to the UID and the user name shows up on the profile page

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -1,1 +1,69 @@
+//Authentication and pulls the unique userID from the database
+function sayHello() {
+    firebase.auth().onAuthStateChanged(function (somebody) {
+        if (somebody) {
+            console.log(somebody.uid);
+            db.collection("users")
+                .doc(somebody.uid)
+                .get()
+                .then(function (doc) {
+                    //console.log(doc.data().name);
+                    var n = doc.data().name;
+                    $("#name-goes-here").text(n);
+                    //get other things and do other things per this user.
+                })
+        }
+    })
+}
+sayHello();
 
+
+//Listen for for submit in profile post form
+document.getElementById('userPost').addEventListener('submit', postForm);
+// Submit form function
+function postForm(e) {
+    e.preventDefault();
+    //Get Values from the post
+    var post = document.getElementById('post').value;
+    console.log(post);
+    writePostToDb;
+
+    function writePostToDb() {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                console.log(user.uid);
+                db.collection("users")
+                .doc(user.uid)
+                .get()
+                .then(function(doc){
+                    console.log(doc.data().name);                
+                    db.collection("posts").add( {userpost: post, userName: doc.data().name, date: date, time: time});
+                    
+                })
+            }
+
+        })
+    }
+    writePostToDb();
+}
+// getting the current date and time
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+//
+
+
+//             //displaying content and writting it to first comment box..
+//             function displayContent() {
+//                 db.collection("posts").get()
+//                     .then(function (snap) {
+//                         snap.forEach(function (doc) {
+//                             var m = doc.data().userPost;             //gets the name field
+//                             console.log(m);
+//                             document.getElementById(userPost).innerText = m;
+//                         })
+
+//                     })
+//             }
+//             displayContent()
