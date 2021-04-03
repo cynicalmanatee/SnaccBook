fa936ec57f3131115975e1ad0b6c92fcc5180d4a
Harry He <harry.he.zz@gmail.com>
2021-04-02 19:43:47 -0700

    WIP

--- scripts/profile-form.js
+++ scripts/profile-form.js
@@ -1,44 +1,63 @@
 $(document).ready(function () {
-
-
-    var users = db.collections("users");
-    var user;
-
     firebase.auth().onAuthStateChanged(function (somebody) {
+        var users = db.collection("users");
+        var user;
         user = somebody.uid;
+        console.log(user);
+
+        var fName = db.collection("users").doc(user)
+            .get()
+            .then(function (doc) {
+                fName = doc.data().name;
+                return fName;
+
+            });
+        console.log(fName);
+
+        var lName;
+        var mName;
+        var pronoun;
+        var uid;
+        var jobTitle;
+        var bio;
+        var skills;
+        var location;
+        var email;
+        var website;
+        var facebook;
+        var twitter;
+        var instagram;
+        var reddit;
+
+        $("#submit-button").click(function (submit) {
+            submit.preventDefault();
+
+            fName = $("#profile-first-name").val();
+            lName = $("#profile-last-name").val();
+            mName = $("#profile-middle-name").val();
+            pronoun = $("#profile-title").val();
+
+
+            var name = fName + " " + mName + " " + lName;
+            console.log(name);
+
+            uid = "";
+
+            jobTitle = $("#profile-job-title").val();
+            bio = $("#profile-bio-description").val();
+            skills = $("#profile-skills").val();
+            location = $("#profile-location").val();
+            email = $("#profile-location").val();
+            website = $("#profile-location").val();
+            facebook = $("#profile-location").val();
+            twitter = $("#profile-location").val();
+            instagram = $("#profile-location").val();
+            reddit = $("#profile-location").val();
+
+
+
+
+        });
+
     });
-    console.log(user);
-
-    $("#submit-button").click(function (submit) {
-        submit.preventDefault();
-
-        var fName = $("#profile-first-name").val();
-        var lName = $("#profile-last-name").val();
-        var mName = $("#profile-middle-name").val();
-        var pronoun = $("#profile-title").val();
-
-
-        var name = fName + " " + mName + " " + lName;
-        console.log(name);
-
-        var uid = "";
-
-        var jobTitle = $("#profile-job-title").val();
-        var bio = $("#profile-bio-description").val();
-        var skills = $("#profile-skills").val();
-        var location = $("#profile-location").val();
-        var email = $("#profile-location").val();
-        var website = $("#profile-location").val();
-        var facebook = $("#profile-location").val();
-        var twitter = $("#profile-location").val();
-        var instagram = $("#profile-location").val();
-        var reddit = $("#profile-location").val();
-
-
-
-
-
-    });
-
-
 });
