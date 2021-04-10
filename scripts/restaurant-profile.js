f2c59cabb3cb3a686f2d18104d889f806b553ed0
Joshua Martinez <josh123198@gmail.com>
2021-04-09 22:00:43 -0700

    currently fixing issues with css files mainly around navbar to fix weird whitespace issues

--- scripts/restaurant-profile.js
+++ scripts/restaurant-profile.js
@@ -6,45 +6,45 @@
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
     var showC1 = { 'grid-template-columns': '100% 0% 0%' };
     var showC2 = { 'grid-template-columns': '0% 100% 0%' };
     var showC3 = { 'grid-template-columns': '0% 0% 100%' };
 
-    $("#tab1").mouseenter(function () {
+    $("#tab1").mouseenter(function() {
         $('#tab1').css({
 
             'color': 'white'
         });
     });
-    $('#tab1').mouseleave(function () {
+    $('#tab1').mouseleave(function() {
         $('#tab1').css({
             'color': '#bed7fa'
         });
     });
-    $("#tab2").mouseenter(function () {
+    $("#tab2").mouseenter(function() {
         $('#tab2').css({
             'color': 'white'
         });
     });
-    $('#tab2').mouseleave(function () {
+    $('#tab2').mouseleave(function() {
         $('#tab2').css({
             'color': '#bed7fa'
         });
     });
-    $("#tab3").mouseenter(function () {
+    $("#tab3").mouseenter(function() {
         $('#tab3').css({
             'color': 'white'
         });
     });
-    $('#tab3').mouseleave(function () {
+    $('#tab3').mouseleave(function() {
         $('#tab3').css({
             'color': '#bed7fa'
         });
     });
 
-    $("#tab1").click(function () {
+    $("#tab1").click(function() {
         $("#content").css(showC1);
         $("#content2").css('visibility', 'hidden');
         $("#content3").css('visibility', 'hidden');
@@ -53,7 +53,7 @@ $(document).ready(function () {
         $("#tab2 span").css('text-shadow', '0px 0 black, 0 0px black, 0px 0 black, 0 0px black');
         $("#tab3 span").css('text-shadow', '0px 0 black, 0 0px black, 0px 0 black, 0 0px black');
     });
-    $("#tab2").click(function () {
+    $("#tab2").click(function() {
         $("#content").css(showC2);
         $("#content1").css('visibility', 'hidden');
         $("#content3").css('visibility', 'hidden');
@@ -62,7 +62,7 @@ $(document).ready(function () {
         $("#tab1 span").css('text-shadow', '0px 0 black, 0 0px black, 0px 0 black, 0 0px black');
         $("#tab3 span").css('text-shadow', '0px 0 black, 0 0px black, 0px 0 black, 0 0px black');
     });
-    $("#tab3").click(function () {
+    $("#tab3").click(function() {
         $("#content").css(showC3);
         $("#content2").css('visibility', 'hidden');
         $("#content1").css('visibility', 'hidden');
