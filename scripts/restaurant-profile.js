3d5c625de3dbf95c25e544545c2d71e5c3fdc693
Joshua Martinez <josh123198@gmail.com>
2021-04-10 22:08:27 -0700

    made updates to CSS of index, profile form, profile, and rest. profile. Made slight changes to landing page and login so navbar is unavailable until user signs in.

--- scripts/restaurant-profile.js
+++ scripts/restaurant-profile.js
@@ -6,57 +6,56 @@
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-    var showC1 = { 'grid-template-columns': '100% 0% 0%' };
-    var showC2 = { 'grid-template-columns': '0% 100% 0%' };
-    var showC3 = { 'grid-template-columns': '0% 0% 100%' };
+    var showC1 = { 'grid-template-columns': '1fr 0fr 0fr' };
+    var showC2 = { 'grid-template-columns': '0fr 1fr 0fr' };
+    var showC3 = { 'grid-template-columns': '0fr 0fr 1fr' };
 
-    $("#tab1").mouseenter(function () {
+    $("#tab1").mouseenter(function() {
         $('#tab1').css({
-
-            'color': 'white'
+            'color': 'grey'
         });
     });
-    $('#tab1').mouseleave(function () {
+    $('#tab1').mouseleave(function() {
         $('#tab1').css({
             'color': 'black'
         });
     });
-    $("#tab2").mouseenter(function () {
+    $("#tab2").mouseenter(function() {
         $('#tab2').css({
-            'color': 'white'
+            'color': 'grey'
         });
     });
-    $('#tab2').mouseleave(function () {
+    $('#tab2').mouseleave(function() {
         $('#tab2').css({
             'color': 'black'
         });
     });
-    $("#tab3").mouseenter(function () {
+    $("#tab3").mouseenter(function() {
         $('#tab3').css({
-            'color': 'white'
+            'color': 'grey'
         });
     });
-    $('#tab3').mouseleave(function () {
+    $('#tab3').mouseleave(function() {
         $('#tab3').css({
             'color': 'black'
         });
     });
 
-    $("#tab1").click(function () {
+    $("#tab1").click(function() {
         $("#content").css(showC1);
         $("#content2").css('visibility', 'hidden');
         $("#content3").css('visibility', 'hidden');
         $("#content1").css('visibility', 'visible');
     });
-    $("#tab2").click(function () {
+    $("#tab2").click(function() {
         $("#content").css(showC2);
         $("#content1").css('visibility', 'hidden');
         $("#content3").css('visibility', 'hidden');
         $("#content2").css('visibility', 'visible');
     });
-    $("#tab3").click(function () {
+    $("#tab3").click(function() {
         $("#content").css(showC3);
         $("#content2").css('visibility', 'hidden');
         $("#content1").css('visibility', 'hidden');
@@ -77,7 +76,7 @@ function displayDetails() {
     db.collection("restaurants")
         .doc(id)
         .get()
-        .then(function (doc) {
+        .then(function(doc) {
 
             var restName = doc.data().name;
             var motto = doc.data().motto;
@@ -86,8 +85,8 @@ function displayDetails() {
             var province = doc.data().province;
             var postalcode = doc.data().postalCode;
 
-            var fullAddress = "" + address + "<br/>" + city + ", " + province
-                + '<br/>' + postalcode;
+            var fullAddress = "" + address + "<br/>" + city + ", " + province +
+                '<br/>' + postalcode;
 
             $("#bannerName").html(restName);
             $("#bannerSubtitle").html(fullAddress);
@@ -178,5 +177,3 @@ function postForm(e) {
 var today = new Date();
 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-
